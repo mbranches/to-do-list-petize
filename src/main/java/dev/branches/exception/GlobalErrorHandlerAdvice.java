74814a625b888fd989c2788393e5efd55c540e78
mbranches <marcusvbranches@gmail.com>
2025-08-11 16:53:49 -0300

    feat: adiciona tratamento de exceção de validação

--- src/main/java/dev/branches/exception/GlobalErrorHandlerAdvice.java
+++ src/main/java/dev/branches/exception/GlobalErrorHandlerAdvice.java
@@ -1,10 +1,14 @@
 package dev.branches.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalErrorHandlerAdvice {
@@ -14,10 +18,25 @@ public class GlobalErrorHandlerAdvice {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
+
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<DefaultErrorMessage> handlerBadRequestException(BadRequestException e) {
         DefaultErrorMessage error = new DefaultErrorMessage(e.getStatusCode().value(), e.getReason());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<DefaultErrorMessage> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String defaultMessage = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .filter(Objects::nonNull)
+                .sorted()
+                .collect(Collectors.joining(", "));
+
+        DefaultErrorMessage response = new DefaultErrorMessage (HttpStatus.BAD_REQUEST.value(), defaultMessage);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }

e9874e2769403403f28f2991833be81ccc77e71a
mbranches <marcusvbranches@gmail.com>
2025-08-12 14:17:03 -0300

    test: adiciona testes unitários para método listAll de TaskService

--- src/test/java/dev/branches/service/TaskServiceTest.java
+++ src/test/java/dev/branches/service/TaskServiceTest.java
@@ -2,14 +2,23 @@ package dev.branches.service;
 
 import dev.branches.entity.Task;
 import dev.branches.entity.TaskStatus;
+import dev.branches.entity.User;
 import dev.branches.repository.TaskRepository;
 import dev.branches.utils.TaskUtils;
+import dev.branches.utils.UserUtils;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -66,5 +75,59 @@ class TaskServiceTest {
         assertThat(response.getStatus())
                 .isNotNull()
                 .isEqualTo(TaskStatus.PENDENTE);
+    }
+
+    @Test
+    @DisplayName("listAll returns all the requesting user tasks when successful")
+    @Order(3)
+    void listAll_ReturnsAllTheRequestingUserTasks_WhenSuccessful() {
+        User userToGiven = taskList.getFirst().getUser();
+        List<Task> userTasks = taskList.stream().filter(task -> task.getUser().equals(userToGiven)).toList();
+
+        PageRequest pageRequest = PageRequest.of(0, userTasks.size());
+
+        PageImpl<Task> expectedResponse = new PageImpl<>(userTasks, pageRequest, userTasks.size());
+
+        when(repository.findAll(ArgumentMatchers.any(Specification.class), ArgumentMatchers.any(Pageable.class)))
+                .thenReturn(expectedResponse);
+
+        Page<Task> response = service.listAll(pageRequest, userToGiven, null, null, null, null);
+
+        assertThat(response)
+                .isNotNull();
+        assertThat(response.getTotalElements())
+                .isEqualTo(userTasks.size());
+        assertThat(response.getSize())
+                .isEqualTo(userTasks.size());
+        assertThat(response.getContent())
+                .isNotNull()
+                .isNotEmpty()
+                .containsExactlyElementsOf(userTasks);
+    }
+
+    @Test
+    @DisplayName("listAll returns an empty list when no tasks are found")
+    @Order(4)
+    void listAll_ReturnsEmptyList_WhenNoTasksAreFound() {
+        User userWhoHasNoTasks = UserUtils.newUserList().get(1);
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        PageImpl<Task> expectedResponse = new PageImpl<>(Collections.emptyList(), pageRequest, 0);
+
+        when(repository.findAll(ArgumentMatchers.any(Specification.class), ArgumentMatchers.any(Pageable.class)))
+                .thenReturn(expectedResponse);
+
+        Page<Task> response = service.listAll(pageRequest, userWhoHasNoTasks, null, null, null, null);
+
+        assertThat(response)
+                .isNotNull();
+        assertThat(response.getTotalElements())
+                .isEqualTo(0);
+        assertThat(response.getSize())
+                .isEqualTo(10);
+        assertThat(response.getContent())
+                .isNotNull()
+                .isEmpty();
     }
 }
